cb589a49f507bbb1ff2bc9d5f0e41533412cb2d1
Fair94 <due.a.muzzo@gmail.com>
2025-12-05 09:34:24 +0100

    Initial Commit: project setup - erd -readme
    In this initial commit I create the project using springinitializr.
    I started creating the readme file and the erd for the entity and relations of the project

---
+++ library365/src/test/java/com/giuseppecalvaruso/library365/Library365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giuseppecalvaruso.library365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Library365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
